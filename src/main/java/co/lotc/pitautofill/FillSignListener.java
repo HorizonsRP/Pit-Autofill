5c5e919efcc7e48efbabefe70e66e6f67f8b5c75
Matthew Hildebrand <korvic@live.ca>
2019-07-26 22:00:29 -0500

    Added safety checking for refill sign placement.

--- src/main/java/co/lotc/pitautofill/FillSignListener.java
+++ src/main/java/co/lotc/pitautofill/FillSignListener.java
@@ -1,14 +1,18 @@
 package co.lotc.pitautofill;
 
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class FillSignListener implements Listener {
+
+	private final static String SIGN_IDENTIFIER = "[Refill]";
 
 	@EventHandler
 	public void OnSignRightclick(PlayerInteractEvent event) {
@@ -21,8 +25,22 @@ public class FillSignListener implements Listener {
 			 block.getType().equals(Material.SIGN))) {
 
 			Sign sign = (Sign) block.getState();
-			if (sign.getLine(1).equalsIgnoreCase("[Refill]")) {
+			if (sign.getLine(1).equalsIgnoreCase(SIGN_IDENTIFIER)) {
 				event.getPlayer().sendMessage(PitAutofill.PREFIX + PitList.fillPit(sign.getLine(2).replace(' ', '_')));
+			}
+		}
+	}
+
+	// Updates a sign as needed if it matches upon update.
+	@EventHandler
+	public void onSignPlaced(SignChangeEvent event) {
+		if (SIGN_IDENTIFIER.equalsIgnoreCase(event.getLine(1))) {
+			if (event.getPlayer().hasPermission("pit.edit")) {
+				event.setLine(1, SIGN_IDENTIFIER);
+				event.setLine(2, WordUtils.capitalizeFully(event.getLine(2).replace('_', ' ')));
+			} else {
+				event.setCancelled(true);
+				event.getPlayer().sendMessage(PitAutofill.PREFIX + "You cannot create a refill sign.");
 			}
 		}
 	}
